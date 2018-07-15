28114cf9727a73181732248a496d700cf1a72bf1
eeyellow <eeyellow@gmail.com>
2018-07-15 19:51:57 +0800

    ✨ Feat: 	 WMS支援1.1.0, 1.1.1, 1.3.0，修正WMS偏移問題

--- src/gf.Map.Core.js
+++ src/gf.Map.Core.js
@@ -418,14 +418,35 @@ function GEEMap(_map) {
                 var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor));
 
                 //corrections for the slight shift of the SLP (mapserver)
-                var deltaX = 0.0013;
-                var deltaY = 0.00058;
+                //var deltaX = 0.0013;
+                //var deltaY = 0.00058;
+                var deltaX = 0;
+                var deltaY = 0;
 
-                //create the Bounding box string
-                var bbox = (top.lng() + deltaX) + "," +
-                    (bot.lat() + deltaY) + "," +
-                    (bot.lng() + deltaX) + "," +
-                    (top.lat() + deltaY);
+                var fixtoplng = top.lng() + deltaX;
+                var fixtoplat = top.lat() + deltaY;
+                var fixbotlng = bot.lng() + deltaX;
+                var fixbotlat = bot.lat() + deltaY;
+                var newtop;
+                var newbot;
+                var version = getParameterByName("version", wmsurl.toLowerCase());
+                switch (version) {
+                    case "1.1.0":
+                    case "1.1.1":
+                        newtop = proj4("EPSG:3857", "EPSG:3857", {x: fixtoplng, y: fixtoplat});
+                        newbot = proj4("EPSG:3857", "EPSG:3857", {x: fixbotlng, y: fixbotlat});
+                        break;
+                    case "1.3.0":
+                        newtop = proj4("EPSG:4326", "EPSG:3857", {x: fixtoplng, y: fixtoplat});
+                        newbot = proj4("EPSG:4326", "EPSG:3857", {x: fixbotlng, y: fixbotlat});
+                        break;
+                }
+                var bbox =
+                    newtop.x + "," +
+                    newbot.y + "," +
+                    newbot.x + "," +
+                    newtop.y;
+
 
                 //base WMS URL
                 var urlarr = wmsurl.split('?');
@@ -438,37 +459,17 @@ function GEEMap(_map) {
                             break;
                         case "height":
                             break;
-                        case "srs":
-                            break;
-                        case "bgcolor":
-                            break;
-                        case "transparent":
+                        case "bbox":
                             break;
                         default:
                             url += kvo[0] + '=' + kvo[1] + '&';
                             break;
                     }
                 });
-                url += "&BGCOLOR=0xFFFFFF";
-                url += "&TRANSPARENT=TRUE";
-                url += "&SRS=EPSG:4326"; //set WGS84
                 url += "&BBOX=" + bbox; // set bounding box
                 url += "&WIDTH=256"; //tile size in google
                 url += "&HEIGHT=256";
-                /*
-                url += "&REQUEST=GetMap"; //WMS operation
-                url += "&SERVICE=WMS";    //WMS service
-                url += "&VERSION=1.1.1";  //WMS version
-                //url += "&LAYERS=" + "typologie,hm2003"; //WMS layers
-                url += "&LAYERS=" + "swcb:TaipeiCity"; //WMS layers
-                url += "&FORMAT=image/png" ; //WMS format
-                url += "&BGCOLOR=0xFFFFFF";
-                url += "&TRANSPARENT=TRUE";
-                url += "&SRS=EPSG:4326";     //set WGS84
-                url += "&BBOX=" + bbox;      // set bounding box
-                url += "&WIDTH=256";         //tile size in google
-                url += "&HEIGHT=256";
-                */
+
                 return url; // return URL for the tile
             },
             tileSize: new google.maps.Size(256, 256),
@@ -741,4 +742,14 @@ function GEEMap(_map) {
             locateDataLayer.remove(feature);
         });
     };
+
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
 }
